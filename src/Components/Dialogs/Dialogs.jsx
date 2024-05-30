afbc4d498d9bcb5a8bd78e37639f930071e3b942
arsenpapoyan <arsenpapoyan02@gmail.com>
2024-05-31 03:20:14 +0400

    some changes

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -7,10 +7,10 @@ class Dialogs extends React.Component {
     
     componentDidMount() {
         if(localStorage.getItem('userID') !== this.props.socket.id ) {
+            console.log(this.props.socket);
             this.props.socket.on('response', async (data) => {
                 if(localStorage.getItem('userID') === data.userId) {
                     this.props.sendMessage(data.message, this.props.activeRoom.roomId, data.id, data.userId, null, data.reactionsBtn);
-                    console.log(data);
                 }
                 setTimeout( async () => {
                     await this.props.getMessage(this.props.activeRoom.roomId, data.id, data.userId);
